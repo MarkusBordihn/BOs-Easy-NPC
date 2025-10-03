d07ad8548831ca9e9e103d92e701a5f0baf4cf99
Markus Bordihn <Markus@Bordihn.de>
2025-10-04 00:45:05 +0200

    Fixed Fabric implementation. Updated knight_with_armor_and_weapon.npc.nbt example.

--- core/Common/src/main/java/de/markusbordihn/easynpc/item/configuration/EasyNPCPresetItem.java
+++ core/Common/src/main/java/de/markusbordihn/easynpc/item/configuration/EasyNPCPresetItem.java
@@ -98,6 +98,22 @@ public class EasyNPCPresetItem extends Item {
       if (customNameTag.contains(TEXT_TAG)) {
         return customNameTag.getString(TEXT_TAG).orElse("");
       }
+
+      try {
+        String customNameString = compoundTag.getString(CUSTOM_NAME_TAG).orElse("");
+        if (!customNameString.isEmpty()) {
+          com.google.gson.JsonElement jsonElement =
+              com.google.gson.JsonParser.parseString(customNameString);
+          if (jsonElement.isJsonObject()) {
+            com.google.gson.JsonObject jsonObject = jsonElement.getAsJsonObject();
+            if (jsonObject.has(TEXT_TAG)) {
+              return jsonObject.get(TEXT_TAG).getAsString();
+            }
+          }
+        }
+      } catch (Exception e) {
+        log.debug("Could not parse CustomName as legacy JSON format", e);
+      }
     }
     return null;
   }
