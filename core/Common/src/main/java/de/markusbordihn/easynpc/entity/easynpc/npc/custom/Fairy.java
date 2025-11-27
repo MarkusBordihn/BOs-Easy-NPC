db6deaf8b47b6191e6c2780e2ffb6451b6cdce71
Markus Bordihn <Markus@Bordihn.de>
2025-11-27 10:53:11 +0100

    Fixed Fairy eye height.

--- core/Common/src/main/java/de/markusbordihn/easynpc/entity/easynpc/npc/custom/Fairy.java
+++ core/Common/src/main/java/de/markusbordihn/easynpc/entity/easynpc/npc/custom/Fairy.java
@@ -27,11 +27,9 @@ import de.markusbordihn.easynpc.data.sound.SoundDataSet;
 import de.markusbordihn.easynpc.data.sound.SoundType;
 import de.markusbordihn.easynpc.entity.easynpc.npc.raw.PathfinderMobRaw;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.PathfinderMob;
-import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.control.FlyingMoveControl;
@@ -125,11 +123,6 @@ public class Fairy extends PathfinderMobRaw {
   @Override
   public boolean canFly() {
     return true;
-  }
-
-  @Override
-  protected float getStandingEyeHeight(Pose pose, EntityDimensions dimensions) {
-    return dimensions.height * 0.85F;
   }
 
   @Override
