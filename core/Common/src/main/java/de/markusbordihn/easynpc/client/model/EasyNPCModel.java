0aea4b6792a04f8c4afbf99b1a0eee2b937d28f8
Markus Bordihn <Markus@Bordihn.de>
2026-03-14 00:42:35 +0100

    Fixed NPC targeting logic to include LOOK_AT_OWNER and LOOK_AT_ENTITY_BY_UUID objectives. Improved root rotation checks for default head rotation.

--- core/Common/src/main/java/de/markusbordihn/easynpc/client/model/EasyNPCModel.java
+++ core/Common/src/main/java/de/markusbordihn/easynpc/client/model/EasyNPCModel.java
@@ -27,7 +27,6 @@ import de.markusbordihn.easynpc.data.model.ModelAnimationBehavior;
 import de.markusbordihn.easynpc.data.model.ModelArmPose;
 import de.markusbordihn.easynpc.data.model.ModelPartType;
 import de.markusbordihn.easynpc.data.model.ModelPose;
-import de.markusbordihn.easynpc.data.rotation.CustomRotation;
 import de.markusbordihn.easynpc.entity.LivingEntityManager;
 import de.markusbordihn.easynpc.entity.easynpc.EasyNPC;
 import de.markusbordihn.easynpc.entity.easynpc.data.DisplayAttributeDataCapable;
@@ -102,10 +101,6 @@ public class EasyNPCModel {
       final EasyNPC<?> easyNPC,
       final ModelDataCapable<?> modelData,
       final EasyNPCModelManager modelManager) {
-    CustomRotation rootRotation = modelData.getModelPartRotation(ModelPartType.ROOT);
-    if (rootRotation == null || !rootRotation.locked()) {
-      return;
-    }
     if (modelData.getModelPartRotation(ModelPartType.HEAD).hasChanged()) {
       return;
     }
@@ -118,6 +113,12 @@ public class EasyNPCModel {
         Mth.clamp(Mth.wrapDegrees(living.yHeadRot - living.yBodyRot), -MAX_HEAD_YAW, MAX_HEAD_YAW)
             * DEG_TO_RAD;
     head.xRot = Mth.clamp(living.getXRot(), -MAX_HEAD_PITCH, MAX_HEAD_PITCH) * DEG_TO_RAD;
+
+    ModelPart hat = modelManager.getModelPart(ModelPartType.HAT);
+    if (hat != null && !modelData.getModelPartRotation(ModelPartType.HAT).hasChanged()) {
+      hat.yRot = head.yRot;
+      hat.xRot = head.xRot;
+    }
   }
 
   public static EasyNPC<?> getEasyNPC(final EasyNPCRenderStateExtension extension) {
--- core/Common/src/main/java/de/markusbordihn/easynpc/client/renderer/manager/EntityTypeManager.java
+++ core/Common/src/main/java/de/markusbordihn/easynpc/client/renderer/manager/EntityTypeManager.java
@@ -214,7 +214,16 @@ public class EntityTypeManager {
 
     // Check if entity type is supported and created entity is a PathfinderMob.
     if (!isUnsupportedEntityType(entityType)) {
-      Entity entity = entityType.create(level, EntitySpawnReason.MOB_SUMMONED);
+      Entity entity;
+      try {
+        entity = entityType.create(level, EntitySpawnReason.MOB_SUMMONED);
+      } catch (Exception e) {
+        log.warn(
+            "{} Failed to create entity for type {}: {}", LOG_PREFIX, entityType, e.getMessage());
+        addUnsupportedEntityType(entityType);
+        return null;
+      }
+
       if (entity instanceof PathfinderMob newPathfinderMob) {
         log.debug(
             "{} Registering PathfinderMob {} for {}", LOG_PREFIX, newPathfinderMob, entityType);
