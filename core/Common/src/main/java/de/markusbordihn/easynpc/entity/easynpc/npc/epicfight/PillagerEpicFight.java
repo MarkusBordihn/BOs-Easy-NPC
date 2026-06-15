d4cf909e9f016ea3a5ecad171b716158e6973018
Markus Bordihn <Markus@Bordihn.de>
2026-06-15 15:52:46 +0200

    Fixed crashes related to 3rd mods trying to access non-existing NPCs goals.

--- core/Common/src/main/java/de/markusbordihn/easynpc/entity/easynpc/npc/epicfight/PillagerEpicFight.java
+++ core/Common/src/main/java/de/markusbordihn/easynpc/entity/easynpc/npc/epicfight/PillagerEpicFight.java
@@ -41,6 +41,13 @@ public class PillagerEpicFight extends PillagerRaw {
   }
 
   @Override
+  protected void registerGoals() {
+    super.registerGoals();
+    this.goalSelector.removeAllGoals(goal -> true);
+    this.targetSelector.removeAllGoals(goal -> true);
+  }
+
+  @Override
   public boolean canUseArmor() {
     return true;
   }
--- core/Common/src/main/java/de/markusbordihn/easynpc/entity/easynpc/npc/epicfight/WitchEpicFight.java
+++ core/Common/src/main/java/de/markusbordihn/easynpc/entity/easynpc/npc/epicfight/WitchEpicFight.java
@@ -40,6 +40,13 @@ public class WitchEpicFight extends WitchRaw {
   }
 
   @Override
+  protected void registerGoals() {
+    super.registerGoals();
+    this.goalSelector.removeAllGoals(goal -> true);
+    this.targetSelector.removeAllGoals(goal -> true);
+  }
+
+  @Override
   public boolean canUseArmor() {
     return true;
   }
